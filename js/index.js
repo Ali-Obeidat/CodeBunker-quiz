0a36f0ca39104172fbce6801ea3bbe8e777f22f1
Ali Obeidat <90185197+Ali-Obeidat@users.noreply.github.com>
2021-10-28 20:02:27 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ let getStartBtn = document.querySelector(".getStart-link");
 window.onload = function () {
   if (localStorage.getItem("sessionState") === "true") {
     loginBtn.innerHTML = "Log out";
-    loginBtn.href = "../index.html";
-    getStartBtn.href = "../html/category.html";
+    loginBtn.href = "./index.html";
+    getStartBtn.href = "./html/category.html";
   }
 };
 
